e757ef01177fa7e1f5b955ba54886980645f8481
aditya-tamhane <tamhaneaditya12@gmail.com>
2022-07-24 14:02:16 +0530

    commit to weather-app in react

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": 'eeb965cee5msh95d78ec3694384ep13072bjsn60a61d943601',
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "66dac4827e59433fdb873fb64ca97314"; 
